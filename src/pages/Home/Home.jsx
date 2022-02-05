0087737f7692e14628a995e4abf8eaf87439f773
Ahmed <ahmedreda152@gmail.com>
2022-02-05 16:22:34 +0200

    delete un neccassty js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,26 @@
 import {
   Box, Flex, Grid, Spinner,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import CountryCard from '../../Components/countryCard/CountryCard';
 import SearchBar from '../../Components/HomeSearch/HomeSearchBar';
 import WrappedMap from '../../Components/Map/Map';
 import useGeoLocation from '../../hooks/useGeoLocation';
-import { fetchweatherCurrLoc } from '../../redux/weatherReducer/weatherCurrLocActions';
 
 const Home = () => {
-  const dispatch = useDispatch();
   const [searchValue, setSearchValue] = useState('');
   const location = useGeoLocation();
-  useEffect(() => {
-    dispatch(
-      fetchweatherCurrLoc({
-        lon: location?.coordinates?.lng,
-        lat: location?.coordinates?.lat,
-      }),
-    );
-  }, []);
+
   const { countries } = useSelector((state) => state);
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
   };
   return (
     <Box width="100%" py="20px">
-      <Flex width="100%" justifyContent="center">Your Current Location</Flex>
+      <Flex width="100%" justifyContent="center">
+        Your Current Location
+      </Flex>
       <WrappedMap
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAgdoMN7JveVG28IoGxMmFY12cZnassnU0"
         loadingElement={<div style={{ height: '100%' }} />}
